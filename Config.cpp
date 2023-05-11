9c4d93f97df389e516071a74c08b50e414f981ff
Jan Zagata <janzagata@gmail.com>
2023-05-11 18:53:22 +0200

    initial commit

---
+++ Config.cpp
@@ -0,0 +1,95 @@
+class CfgPatches {
+
+	class TFZAddon
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {};
+		version = "0.0.1";
+		author[]= {"ReFuse"};
+		authorUrl = "";
+	};
+
+};
+
+class CfgUnitInsignia
+{
+	
+	class tfz
+	{
+		displayName = "TF Zaludi";
+		author = "ReFuse";
+		texture = "TFZaddon\textures\insignia.paa";
+		textureVehicle = "";
+
+	};
+	class cjtf_254
+	{
+		displayName = "CJTF 254";
+		author = "ReFuse";
+		texture = "TFZaddon\textures\tf-254.paa";
+		textureVehicle = "";
+
+	};
+	class kandahar_iii
+	{
+		displayName = "Kandahar III";
+		author = "ReFuse";
+		texture = "TFZaddon\textures\kandahar_iii.paa";
+		textureVehicle = "";
+
+	};
+
+    class kandahar_v
+    {
+        displayName = "Kandahar V";
+        author = "ReFuse";
+        texture = "TFZaddon\textures\kandahar_v.paa";
+        textureVehicle = "";
+    };
+
+	class lizzard_xi
+	{
+		displayName = "Lizzard XI";
+		author = "ReFuse";
+		texture = "TFZaddon\textures\lizzard_xi.paa";
+		textureVehicle = "";
+
+	};
+
+	class lizzard_xii
+	{
+		displayName = "Lizzard XII";
+		author = "ReFuse";
+		texture = "TFZaddon\textures\lizzard_xii.paa";
+		textureVehicle = "";
+
+	};
+
+    class lizzard_xiv
+    {
+        displayName = "Lizzard XIV";
+        author = "ReFuse";
+        texture = "TFZaddon\textures\lizzard_xiv.paa";
+        textureVehicle = "";
+
+    };
+
+	class protector_xvii
+	{
+		displayName = "Protector XVII";
+		author = "ReFuse";
+		texture = "TFZaddon\textures\protector_xvii.paa";
+		textureVehicle = "";
+
+	};
+    
+    class pork_crusader
+    {
+        displayName = "Pork eating crusader";
+        author = "ReFuse";
+        texture = "TFZaddon\textures\pork_eating_crusader.paa";
+        textureVehicle = "";
+    };
+	
+};
---
+++ Mod.cpp
@@ -0,0 +1,14 @@
+name 			= "TaskForce Žaludi"; // Name of your mod
+author 		    = "ReFuse"; // Affects Arma 3 Launcher, when the mod are loaded as local
+logo 			= "TFZaddon\textures\logo.paa"; // Logo displayed in the main menu
+logoOver 		= "TFZaddon\textures\logo_hover.paa"; // When the mouse is over, in the main menu
+logoSmall 		= "TFZaddon\textures\logo.paa"; // Display in creative lists, next to the entities added by the mod
+tooltip 		= "TF Žaludi";
+tooltipOwned 	= "TF Žaludi"; // Tool tip displayed when the mouse is left over, in the main menu
+picture 		= "TFZaddon\textures\banner.paa"; // Picture displayed from the expansions menu. Optimal size is 2048x1024
+actionName 	    = "GitHub";
+action 		    = "https://github.com/janzagata/TFZAddon"; // Website URL, that can be accessed from the expansions menu
+overview 		= "Bunch of czech-airsoft patches."; // Supports structured text
+hideName 		= 0; // Hide the extension name
+hidePicture	    = 0; // Hide the extension menu
+dlcColor[] 	    = {0.23,0.39,0.30,1}; // Color used for DLC stripes and backgrounds (RGBA)
